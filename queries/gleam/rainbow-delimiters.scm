48e7b5ceb2d6f5ebf351bc78fdf9dbeafb552395
artsbentley <artsbentley@gmail.com>
2025-03-01 22:45:54 +0100

    gleam

---
+++ queries/gleam/rainbow-delimiters.scm
@@ -0,0 +1,27 @@
+;; For tuple expressions (if you still want a separate rule for type-level tuples,
+;; you might have to merge it with the existing tuple rule, since Gleam only defines `tuple`)
+(tuple
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(function_parameters
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+;; Use `block` instead of `function_body`
+(block
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(case_clause
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(list
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(bit_string
+  "<<" @delimiter
+  ">>" @delimiter @sentinel) @container
+
